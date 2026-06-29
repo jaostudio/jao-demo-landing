0fc68936c5c0bf0f60ab85df4d7f4d2d424ef5f3
jamesonolitoquit <jamesonolitoquit@github.com>
2026-06-29 13:29:19 +0800

    use @jaostudio/*@0.2.0 compiled packages, remove transpilePackages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
-  transpilePackages: ["@jaostudio/core","@jaostudio/engine","@jaostudio/ui"],
 }
 
 export default nextConfig
